c41ed4aa701c06000fa50548d7483c2c07662554
Mohosin <mohosin16103308@gmail.com>
2019-07-05 10:07:31 +0600

    Create Category And Video Model and load Data using Fixtures

---
+++ src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\Category;
+
+class CategoryFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $this->loadMainCategories($manager);
+    }
+
+    private function loadMainCategories($manager)
+    {
+        foreach($this->getMainCategoriesData() as [$name] )
+        {
+            $category = new Category();
+            $category->setName($name);
+            $manager->persist($category);
+        }
+
+        $manager->flush();
+    }
+
+    private function getMainCategoriesData()
+    {
+        return [
+            ['Electronics', 1],
+            ['Toys', 2],
+            ['Books', 3],
+            ['Movies', 4],
+        ];
+    }
+}
+
