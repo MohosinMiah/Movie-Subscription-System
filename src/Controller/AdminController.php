1d50f13bcf8c03550bd61107100d240216f7f4c0
Mohosin <mohosin16103308@gmail.com>
2019-07-06 23:29:47 +0600

    Admin Category Edit part 3

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -33,6 +33,7 @@ class AdminController extends AbstractController
 
         $category = new Category();
         $form = $this->createForm(CategoryType::class, $category);
+        $is_invalid = null;
         $form->handleRequest($request);
 
         if($form->isSubmitted() && $form->isValid())
@@ -40,10 +41,15 @@ class AdminController extends AbstractController
             // save category
             dd('valid');
         }
+        elseif($request->isMethod('post'))
+        {
+            $is_invalid = ' is-invalid';
+        }
 
         return $this->render('admin/categories.html.twig',[
             'categories'=>$categories->categorylist,
-            'form'=>$form->createView()
+            'form'=>$form->createView(),
+            'is_invalid'=>$is_invalid
         ]);
     }
 
