c0743926754916208ae6153e1c1d87c95034342a
Mohosin <mohosin16103308@gmail.com>
2019-07-16 19:19:43 +0600

    Shorted  Categories

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -7,6 +7,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Category;
 use App\Entity\Video;
 use App\Utils\CategoryTreeFrontPage;
+use Symfony\Component\HttpFoundation\Request;
 
 class FrontController extends AbstractController
 {
@@ -21,14 +22,14 @@ class FrontController extends AbstractController
     /**
      * @Route("/video-list/category/{categoryname},{id}/{page}", defaults={"page": "1"}, name="video_list")
      */
-    public function videoList($id, $page, CategoryTreeFrontPage $categories)
+    public function videoList($id, $page, CategoryTreeFrontPage $categories, Request $request)
     {
         $ids = $categories->getChildIds($id);
         array_push($ids, $id);
 
         $videos = $this->getDoctrine()
         ->getRepository(Video::class)
-        ->findByChildIds($ids ,$page);
+        ->findByChildIds($ids ,$page, $request->get('sortby'));
 
         $categories->getCategoryListAndParent($id);
         return $this->render('front/video_list.html.twig',[
